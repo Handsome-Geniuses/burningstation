aecf9f045989bb7e037afae7ff1d74f95e6033ca
wck2 <william.cowles@ipsgroupinc.com>
2026-06-09 08:16:41 -0700

    Let SSHMeter and Meter Manager handle connect/disconnect

--- flask/lib/automation/tests/refresh_meter.py
+++ flask/lib/automation/tests/refresh_meter.py
@@ -6,10 +6,7 @@ from lib.sse.sse_queue_manager import SSEQM as master
 def refresh_meter(meter: SSHMeter, shared: SharedState = None, **kwargs):
     print(f">> REFRESHING METER INFORMATION!")
 
-    isConnected = meter.connected
-    if (not isConnected): meter.connect()
     meter.status='ready'
     info = meter.get_info(force=True)
-    if (not isConnected): meter.close()
     master.broadcast("meter", {"ip": meter.host, "alive": True, "info": info})
     check_stop_event(shared)
